1ae1730cd89725523d51ef9173c6a2fc84815943
lintiancheng <lintiancheng@unittec.com>
2020-05-22 00:30:12 +0800

    初始导入

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+/* 
+ * 日期：2020年5月4日
+ *  
+ * 版权所有：浙江浙大网新众合轨道交通工程有限公司
+ */
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/home")
+    public String hello() {
+        return "hello spring boot!";
+        
+    }
+}
